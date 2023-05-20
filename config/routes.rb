ca9fc32de0834de2b864778b8a4237154c7ad2b5
Yusei <ec2-user@ip-172-31-3-37.ap-northeast-1.compute.internal>
2023-05-20 14:34:22 +0000

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as:'about'
   devise_for :users
   resources :photos , only: [:index,:new,:create,:show,:edit,:update,:destroy] 
+  resources :users, only: [:edit,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
