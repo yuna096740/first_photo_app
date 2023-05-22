335aec5d5a2e320ebc9bdd4c86f79706f41932d7
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-05-22 03:18:13 +0000

    Add relationship model controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,24 @@ class User < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :photo_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_one_attached :icon
-  
-  
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :revers_of_relationships, class_name: "Relationship", foreign_key: "followwed_id" ,dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :revers_of_relationships, source: :follower
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as:'about'
 
-  resources :users, only: [:edit,:show,:update,:destroy]
+  resources :users, only: [:edit,:show,:update,:destroy] do
+    resources :relationships, only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :photos , only: [:index,:new,:create,:show,:edit,:update,:destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :photo_comments, only: [:create,:destroy]
