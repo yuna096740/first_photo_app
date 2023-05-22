8a9c666e2037fac6d9a6e013c13e263b9c9ab301
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-05-22 14:13:30 +0000

    parallax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resourse)
-    about_path
+    photos_path
   end
   def after_sign_out_path_for(resourse)
-    about_path
+    root_path
   end
 
   protected
